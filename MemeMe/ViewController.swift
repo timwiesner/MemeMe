d88aa4b31a6c52198a2d435a9ed9776bf7feb753
Tim Wiesner <timothyjwiesner@gmail.com>
2018-12-11 14:41:39 -0600

    Add camera button

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -11,14 +11,28 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    @IBAction func pickAnImage(_ sender: Any) {
+    @IBAction func pickImageFromAlbum(_ sender: Any) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func pickImageFromCamera(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
         present(imagePicker, animated: true, completion: nil)
     }
     
