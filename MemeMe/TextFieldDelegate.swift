a77c6f164bb6a35e7c9d2f28684dd6154c21f4d0
Tim Wiesner <timothyjwiesner@gmail.com>
2018-12-19 14:24:22 -0600

    transition to textFieldDelegate

---
+++ MemeMe/TextFieldDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  TextFieldDelegate.swift
+//  MemeMe
+//
+//  Created by Tim Wiesner on 12/14/18.
+//  Copyright © 2018 Tim Wiesner. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TextFieldDelegate : NSObject, UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == "TOP" || textField.text == "BOTTOM" {
+            textField.text = ""
+        }
+    }
+}
--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -19,25 +19,35 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
     
-    
-    let memeTextAttributes: [NSAttributedString.Key: Any] = [
-        NSAttributedString.Key.strokeColor: UIColor.black,
-        NSAttributedString.Key.foregroundColor: UIColor.white,
-        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-        NSAttributedString.Key.strokeWidth:  -3.5
-    ]
+    var textFieldDelegate = TextFieldDelegate()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        topTextField.defaultTextAttributes = memeTextAttributes
-        bottomTextField.defaultTextAttributes = memeTextAttributes
         topTextField.text = "TOP"
         bottomTextField.text = "BOTTOM"
-        topTextField.textAlignment = .center
-        bottomTextField.textAlignment = .center
-        self.topTextField.delegate = self
-        self.bottomTextField.delegate = self
         shareButton.isEnabled = false
+    }
+    
+    func initText(_ textField: UITextField) {
+        textField.delegate = textFieldDelegate
+        
+        let memeTextAttributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedString.Key.strokeWidth:  -3.5
+        ]
+        
+//        topTextField.defaultTextAttributes = memeTextAttributes
+//        bottomTextField.defaultTextAttributes = memeTextAttributes
+        textField.defaultTextAttributes = memeTextAttributes
+        textField.textAlignment = .center
+//        topTextField.text = "TOP"
+//        bottomTextField.text = "BOTTOM"
+//        topTextField.textAlignment = .center
+//        bottomTextField.textAlignment = .center
+//        self.topTextField.delegate = self
+//        self.bottomTextField.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
